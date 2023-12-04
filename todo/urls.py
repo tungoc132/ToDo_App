8abe31c943974656d9595499af6df70f3f34cb1c
hthuongg511 <147119293+hthuongg511@users.noreply.github.com>
2023-12-04 08:24:06 +0700

    Update urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
     path('task-edit/<int:pk>/', TaskEdit.as_view(), name='task-edit'),
     path('deletetask/<int:pk>/', TaskDelete.as_view(), name='deletetask'),
 
-    path('month_with_schedule/', views.MonthWithScheduleCalendar.as_view(), name='month_with_schedule'),
-    path('month_with_forms/', views.MonthWithFormCalendar.as_view(), name='month_with_forms'),
-    path('mycalendar/', views.MyCalendar.as_view(), name='mycalendar'),
+    path('', views.MonthCalendar.as_view(), name='month'),
+    path('month/<int:year>/<int:month>/', views.MonthCalendar.as_view(), name='month'),
 ]
